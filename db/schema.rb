9fd436f3c44cfca29bf13c5ca464049ad92fb056
peterpih <ppih@panix.com>
2015-10-03 19:47:37 +0100

    Changed database from sql3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150905235745) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
